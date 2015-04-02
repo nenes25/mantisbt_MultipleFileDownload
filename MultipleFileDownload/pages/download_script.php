120ab976b03fc8e5c95a349594b69c344e8dd943
Hervé <nenes_fr@yahoo.fr>
2015-04-02 12:32:46 +0200

    Gestion avec methode d'upload DISK et FTP // Compatible with DISK and FTP upload Method

--- MultipleFileDownload/pages/download_script.php
+++ MultipleFileDownload/pages/download_script.php
@@ -17,6 +17,7 @@ $t_files_download = gpc_get_int_array('files');
 
 #Donées de configuration mantis
 $t_upload_method = config_get('file_upload_method');
+$t_upload_directory = config_get('absolute_path_default_upload_folder');
 $t_bug_file_table = db_get_table('mantis_bug_file_table');
 
 #Nom de l'archive qui va être créée
@@ -35,10 +36,12 @@ if ($archive->open($t_file_name_with_path, ZipArchive::CREATE) === TRUE) {
         $result = db_query_bound($query, array($t_file_download));
         $row = db_fetch_array($result);
 
-        #Ajout des donnes à l'archive(@ToDO gérer en fonction de la méthode de stockage des fichiers)
-        //echo 'Ajout du fichier '. $row['filename'] . ' a l\archive' . "\n";
-        $archive->addFromString($row['filename'], $row['content']);
-        //$archive->addFile($t_upload_directory . '/' . $row['diskfile'], $row['filename']);
+        #Ajout des donnes à l'archive
+        if ( $t_upload_method == DISK || $t_upload_method == FTP )
+          $archive->addFile($t_upload_directory  . $row['diskfile'], $row['filename']);
+        #Sinon si on est en mode DB ajout du contenu à l'archive
+        else 
+            $archive->addFromString($row['filename'], $row['content']);
     }
     $archive->close();
     
@@ -46,18 +49,18 @@ if ($archive->open($t_file_name_with_path, ZipArchive::CREATE) === TRUE) {
     header('Pragma: public');
     header('Expires: 0');
     header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime('test.zip')) . ' GMT');
-    header('Content-Type: application/force-download');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($t_file_name_with_path)) . ' GMT');
+    header('Content-type: application/zip');
     header('Content-Disposition: inline; filename="'.date('Ymd').'files-bug-'.$t_bug_id.'.zip"');
     header('Content-Transfer-Encoding: binary');
     header('Content-Length: ' . filesize($t_file_name_with_path));
     header('Connection: close');
     readfile($t_file_name_with_path);
+    #supression du fichier
+    unlink($t_file_name_with_path);
+    #Fin du script
     exit();
-    
-    #Suppression du fichier
-    //unlink($t_file_name_with_path);
 }
 else {
-    echo 'Erreur durant la creation de l\'archive de téléchargement';
+    echo plugin_lang_get( 'error_unable_to_create_zip_file' );
 }
